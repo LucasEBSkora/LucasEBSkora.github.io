7e5f4ea24dab053023580259074a8cf463498ec9
LucasEBSkora <skora.lucas@gmail.com>
2020-06-12 21:23:25 -0300

    makes it actually kinda work

--- dev_diary.js
+++ dev_diary.js
@@ -2,10 +2,13 @@ let entries = ["11_06_2020.html"]
 
 let section = document.getElementById('contents')
 
-for (entry in entries) {
-  let doc = fetch(entry)
-  let element = Element()
-  element.class = "diary_entry"
-  element.innerHTML = doc
-  section.appendChild(element)
+for (let entry in entries) {
+  
+  let doc = fetch("dev_diary/" + entries[0]).then((value) => value.text()).then((text) => {
+
+    let element = document.createElement("div")
+    element.class = "diary_entry"
+    element.innerHTML = text
+    section.appendChild(element)
+  })
 }
