5cd5fd6d42444ddc3aa89b0cb420342641e850b5
LucasEBSkora <skora.lucas@gmail.com>
2020-06-12 21:16:25 -0300

    adds full 11/0/2020 entry

--- dev_diary.js
+++ dev_diary.js
@@ -2,4 +2,10 @@ let entries = ["11_06_2020.html"]
 
 let section = document.getElementById('contents')
 
-section.appendChild
+for (entry in entries) {
+  let doc = fetch(entry)
+  let element = Element()
+  element.class = "diary_entry"
+  element.innerHTML = doc
+  section.appendChild(element)
+}
